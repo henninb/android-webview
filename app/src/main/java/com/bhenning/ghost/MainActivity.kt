3dee5db4af730adaddcb545f9cb73e89f008003c
Brian Henning <henninb@msn.com>
2023-09-04 12:14:51 -0500

    wip

--- app/src/main/java/com/bhenning/ghost/MainActivity.kt
+++ app/src/main/java/com/bhenning/ghost/MainActivity.kt
@@ -1,24 +1,38 @@
 package com.bhenning.ghost
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mywebView: WebView
 
+    @SuppressLint("SetJavaScriptEnabled")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mywebView = findViewById(R.id.webview)
         mywebView.webViewClient = WebViewClient()
-        mywebView.loadUrl("https://www.example.com/")
-        val webSettings: WebSettings = mywebView.settings
-        webSettings.javaScriptEnabled = true
+        mywebView.apply {
+            loadUrl("https://www.google.com/")
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+        }
+    }
 
+    override fun onBackPressed() {
+        if (mywebView.canGoBack()) {
+            mywebView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
